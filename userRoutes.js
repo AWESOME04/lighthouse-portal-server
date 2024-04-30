beb2ad3273a8b21899accb42297489b0d7dfc2c0
Evans Acheampong <evansachie01@gmail.com>
2024-04-30 16:18:19 +0000

    modified userRoutes

--- userRoutes.js
+++ userRoutes.js
@@ -5,7 +5,7 @@ module.exports = (pool) => {
     const router = express.Router();
 
     // GET route to fetch the user's email
-    router.get('/me', async (req, res) => {
+    router.get('/', async (req, res) => {
         try {
             const token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, 'your_secret_key');
