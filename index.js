2fd9940221998713f93607b67e0a6559c1bc6185
Evans Acheampong <evansachie01@gmail.com>
2024-05-12 23:00:05 +0000

    fixing caloriesRoutes.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use('/api/users', userRoutes(pool));
 app.use('/api/hydration', hydrationRoutes(pool));
 
 // Calories routes
-app.use('/api/calories', caloriesRoutes());
+app.use('/api/calories', caloriesRoutes(pool));
 
 // Settings routes
 app.use('/api/settings', settingsRoutes(pool));
