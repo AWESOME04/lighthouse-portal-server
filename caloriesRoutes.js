fb9258f44e1c75958dfd365c4c2e73725ec4d1ef
Evans Acheampong <evansachie01@gmail.com>
2024-05-12 23:16:06 +0000

    added get method for coloriesRoutes

--- caloriesRoutes.js
+++ caloriesRoutes.js
@@ -4,6 +4,29 @@ const jwt = require('jsonwebtoken');
 module.exports = (pool) => {
     const router = express.Router();
 
+    router.get('/measurements', async (req, res) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, 'your_secret_key');
+            const user_id = decoded.user_id;
+
+            const { rows } = await pool.query(
+                'SELECT age, weight, height, gender, activity_level, resting_calories, calorie_intake, calories_burned FROM user_measurements WHERE user_id = $1 ORDER BY created_at DESC LIMIT 1',
+                [user_id]
+            );
+
+            if (rows.length > 0) {
+                res.json(rows[0]);
+            } else {
+                res.status(404).json({ error: 'User measurements not found' });
+            }
+        } catch (err) {
+            console.error('Error verifying token:', err);
+            return res.status(500).json({error: 'Internal server error'});
+        }
+    });
+
+
     // POST route to calculate calories
     router.post('/calculate', async (req, res) => {
         try {
@@ -100,6 +123,7 @@ module.exports = (pool) => {
                 return 1.2;
         }
     };
+
 
     return router;
 };
